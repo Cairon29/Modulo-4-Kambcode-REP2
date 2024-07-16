bc560cdcabcfe479095d84012e50fb0a7b1238db
Cairon29 <josewjjunco@gmail.com>
2024-07-16 10:52:58 -0500

    *tarea7* and *FiltersRick-Morty* folders created and *src* folder from *./tarea6/seccionesRick-Morty* inherited on the  added folder to coninue creating

---
+++ tarea7/filtersRick-Morty/src/assets/components/home.jsx
@@ -0,0 +1,9 @@
+export const Home = () => {
+    return (
+        <section>
+            <h1>Hola, aqui deberia haber un home</h1>
+            <h2>pero aun no lo hay</h2>
+            <p>Regresa a tus paginas sospechosas...</p>
+        </section>
+    )
+}
---
+++ tarea7/filtersRick-Morty/src/assets/notFound.jsx
@@ -0,0 +1,5 @@
+export const NotFound = () => {
+    return (
+        <h1>ESCRIBE UNA URL EXISTENTE BOLUDO</h1>
+    )
+}
